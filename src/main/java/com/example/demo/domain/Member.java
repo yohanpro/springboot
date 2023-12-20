2c029f1e808a6e9d7f041b51571fb97492c75f76
김요한(상품상세개발팀) <yohan.kim01@lotte.net>
2023-12-20 09:49:37 +0900

    회원도메인과 리포지토리 만들기

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain;
+
+public class Member {
+    private Long id;
+    private  String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
