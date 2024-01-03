ff092732cb03da79c517ba25a151493f34b18ca5
김요한(상품상세개발팀) <yohan.kim01@lotte.net>
2024-01-03 09:09:15 +0900

    컴포넌트 스캔과 자동의존관계 설정

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 
 import com.example.demo.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-
+@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
