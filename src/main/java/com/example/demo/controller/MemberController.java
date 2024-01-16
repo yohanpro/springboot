54d2a30c2c0fea52f363ec73c6ede80f3128e400
김요한(상품상세개발팀) <yohan.kim01@lotte.net>
2024-01-16 09:05:57 +0900

    회원웹기능 -조회

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class MemberController {
-
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
     private final MemberService memberService;
 
     @Autowired
@@ -23,11 +27,19 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/memebers/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
+        logger.info("Info level log message");
         Member member = new Member();
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping(value="/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
